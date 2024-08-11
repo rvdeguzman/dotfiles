c7d262a5c8c42f59151a5b610edb0f20745c56ca
Rafael Vincent <rafdeguzman1@gmail.com>
2024-08-10 23:58:05 -0400

    created copy config script

---
+++ copy-configs.zsh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+
+CONFIG_DIR="$HOME/.config"
+REPO_DIR="$HOME/repos/dotfiles/"
+
+TARGET_DIR="${1:-$REPO_DIR}"
+
+FOLDERS=(kitty nvim skhd yabai vscode iterm2)
+
+echo "Moving folders from ${CONFIG_DIR} to ${TARGET_DIR}"
+
+# Move the specified folders
+for FOLDER in "${FOLDERS[@]}"; do
+    if [ -d "$CONFIG_DIR/$FOLDER" ]; then
+        echo "Moving $FOLDER to $REPO_DIR"
+        cp -r "$CONFIG_DIR/$FOLDER" "$TARGET_DIR/"
+    else
+        echo "Folder $FOLDER does not exist in $CONFIG_DIR"
+    fi
+done
+
+echo "Done."
