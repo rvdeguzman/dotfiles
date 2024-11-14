07c2c04358874fcb7bf8f2a8065392bafd51ef0b
rafael vincent <rv@shibuya.local>
2024-11-14 13:56:43 -0500

    update

---
+++ config/doom/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
+ '(org-agenda-files
+   '("~/Library/Mobile Documents/com~apple~CloudDocs/school/concordia-refile.org" "/Users/rv/Library/Mobile Documents/com~apple~CloudDocs/school/daily.org" "/Users/rv/Library/Mobile Documents/com~apple~CloudDocs/school/school.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
