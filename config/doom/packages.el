ee015125a1a80913652fc49c4a8202d448003b63
rv <rvdeguzman@protonmail.com>
2025-11-03 20:24:23 -0500

    submodules

--- config/doom/packages.el
+++
@@ -1,8 +0,0 @@
-(package! org-roam-ui
-  :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
-(package! websocket)
-
-;; (unpin! org-roam)
-;; (unpin! websocket)
-
-(package! kanagawa-themes)
